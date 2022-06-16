488f04cb9a905c0b066fa58fa7a996a2a7b32ec8
is-32 kalmykova <zagrebinadarya@gmail.com>
2022-06-16 11:00:29 +0500

    Добавьте файлы проекта.

---
+++ DataBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetClub
+{
+    class DataBase
+    {
+    }
+
+    public boll ExecuteCommand(MySqlCommand command)
+    {
+        bool bl = true;
+        try
+        {
+            Variables.Connection.Open();
+            command.ExecuteNonQuery();
+        }
+        catch (Exception x) {MessageBox }
+    }
+}
---
+++ Variables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DetClub
+{
+    class Variables
+    {
+        public static CurrentUser User = CurrentUser();
+        public static MySqlConnection Connection;
+        public static string filepath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), @"ChildEntertain\Server");
+    }
+}
